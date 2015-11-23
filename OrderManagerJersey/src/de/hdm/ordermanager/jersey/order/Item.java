f3990d113b4f2dc5989c778260bf64445d22ef4b
Andreas Herrmann <andreas.m.herrmann@hotmail.de>
2015-11-23 13:50:54 +0100

    OrderManager Serverteil mit Jersey-Umsetzung

---
+++ OrderManagerJersey/src/de/hdm/ordermanager/jersey/order/Item.java
@@ -0,0 +1,31 @@
+package de.hdm.ordermanager.jersey.order;
+
+public class Item {
+	private String name;
+	private long nummer;
+	private float preis;
+	
+	public Item(){
+		
+	}
+	public Item(String name,long nummer, float preis){
+		this.name=name;
+		this.nummer=nummer;
+		this.preis=preis;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public long getNummer() {
+		return nummer;
+	}
+
+	public float getPreis() {
+		return preis;
+	}
+	public void setNummer(long nummer) {
+		this.nummer = nummer;
+	}
+}
---
+++ OrderManagerJersey/src/de/hdm/ordermanager/jersey/order/Order.java
@@ -0,0 +1,53 @@
+package de.hdm.ordermanager.jersey.order;
+
+import java.util.Vector;
+
+public class Order{
+
+	/**
+	 * 
+	 */
+	private long orderNummer;
+	private float gesamtPreis;
+	private Vector<OrderPosition> orderPositionen = new Vector<OrderPosition>();
+	
+	public Order(){
+		
+	}
+	public Order(long nummer){
+		this.orderNummer=nummer;
+		this.gesamtPreis=0.0F;
+	}
+
+	public long getOrderNummer() {
+		return orderNummer;
+	}
+
+	public float getGesamtPreis() {
+		return gesamtPreis;
+	}
+	
+	public void addPosition(OrderPosition orderPosition){
+		orderPositionen.add(orderPosition);
+		float preis = 0.0F;
+		preis= (orderPosition.getAnzahl()*orderPosition.getItem().getPreis());
+		this.gesamtPreis+=preis;
+	}
+	
+	public Vector<OrderPosition> getOrderPositionen() {
+		return orderPositionen;
+	}
+
+	public boolean equals(Order order){
+		if(this.orderNummer==order.getOrderNummer()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	public void setOrderNummer(long orderNummer) {
+		this.orderNummer = orderNummer;
+	}
+}
